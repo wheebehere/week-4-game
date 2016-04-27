33a553b55a5e272bf197e8f9b6eaed5d064e3fde
Susan Hooper <hoopersusane@gmail.com>
2016-04-27 12:34:11 -0400

    update CC game to include initial coding changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+	$('#crystalOne').on('click'), crystalOne
+}
+function randomNumberBox(){
+
+}
+
+function scoreBox(){
+
+}
+
+function totalScoreBox(){
+
+}
+
+function crystalOne(){
+
+}
+
+function crystalTwo(){
+	
+}
+
+function crystalThree(){
+	
+}
+
+function crystalFour(){
+	
+}
